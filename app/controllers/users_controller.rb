026714fb18e351ca68f264014b71854613006435
shubhsk88 <shubham88singh@gmail.com>
2020-06-11 01:44:58 +0530

    Adding the signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+    def new
+        @user=User.new
+    end
+    def index
+        @users=User.all
+    end
+
+
+    def show
+        @user=User.find(params[:id])
+    end   
+
+    def create
+        @user=User.new(user_params)
+        if @user.save
+            redirect_to @user,Notice:"Sign Up Successful" 
+        else 
+            render :new
+        end
+    end
+
+
+    private
+
+    def user_params
+       params.require(:user).permit(:username,:name,:email,:password)
+    end
 end
