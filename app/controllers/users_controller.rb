56825b0af56713afea405bf4156cec05de6541e0
shubhsk88 <shubham88singh@gmail.com>
2020-06-11 22:14:02 +0530

    Adding the styling the creating other pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,15 @@ class UsersController < ApplicationController
 
         @user=User.new(user_params)
         if(@user.save)
-            redirect_to user_path(@user)
-
+            redirect_to user_path(@user), notice: "Thanks for signing up!"
+        else 
+            render :new
         end
+    end
+
+    def show
+        @user=User.find(params[:id])
+
     end
     
 
