027d556eb49b0e2184a1ff613b80a5c2703ffae5
shubhsk88 <shubham88singh@gmail.com>
2020-06-11 21:30:50 +0530

    Setting up the index page of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,31 @@
 class UsersController < ApplicationController
     def index
-        
+     @users=User.all    
     end
 
     def new
+
+        @user=User.new
     end
 
     def create
+
+        @user=User.new(user_params)
+        if(@user.save)
+            redirect_to user_path(@user)
+
+        end
     end
     
 
     def destroy
     end
 
+    private
 
-    
+    def user_params
+        params.require(:user).permit(:name,:email,:password)
+    end
+
+
 end
