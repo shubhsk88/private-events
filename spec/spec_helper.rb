441f4428cccaca07139795f0bcf640505b82644c
shubhsk88 <shubham88singh@gmail.com>
2020-06-15 05:24:45 +0530

    Fixing the linter errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,22 +36,22 @@ RSpec.configure do |config|
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
-  RSpec.configure do |config|
-    config.expect_with :rspec do |expectations|
+  RSpec.configure do |confige|
+    confige.expect_with :rspec do |expectations|
       expectations.include_chain_clauses_in_custom_matcher_descriptions = true
     end
 
-    config.mock_with :rspec do |mocks|
+    confige.mock_with :rspec do |mocks|
       mocks.verify_partial_doubles = true
     end
 
-    config.shared_context_metadata_behavior = :apply_to_host_groups
-    config.filter_run_when_matching :focus
-    config.example_status_persistence_file_path = 'spec/examples.txt'
-    config.disable_monkey_patching!
-    config.default_formatter = 'doc' if config.files_to_run.one?
-    config.order = :random
-    Kernel.srand config.seed
+    confige.shared_context_metadata_behavior = :apply_to_host_groups
+    confige.filter_run_when_matching :focus
+    confige.example_status_persistence_file_path = 'spec/examples.txt'
+    confige.disable_monkey_patching!
+    confige.default_formatter = 'doc' if confige.files_to_run.one?
+    confige.order = :random
+    Kernel.srand confige.seed
     # config.profile_examples = 10
   end
 end
